a2642162d724dbe80c2f08bc2b0e28ca45ada09c
John Komban <johnpk007@gmail.com>
2020-05-16 01:27:15 -0400

    Contact component added to school- Day#7

--- src/screens/school/index.jsx
+++ src/screens/school/index.jsx
@@ -5,26 +5,30 @@ import { makeStyles } from '@material-ui/core/styles'
 import MUIDatatable from 'mui-datatables';
 import PageHeader from '../../common/components/PageHeader';
 import Address from '../../common/components/Address';
+import Contact from '../../common/components/contact';
 import { getSchoolDetail } from '../../store/actions/school-actions'
-import { Typography } from '@material-ui/core';
+import { Typography, Paper } from '@material-ui/core';
 
-const styles = theme => ({
-    root: {
-        width: '100%'
-    },
-    container: {
-        display: 'flex',
-        flexDirection: 'column',
-        // backgroundColor: 'blue',
-        margin: '10px 20px'
-    },
-    schoolDetails: {
-        height: '30%',
-        padding: '0px 25px',
-        textAlign: 'center',
-    }
+const styles = theme => {
+    console.log(theme)
+    return ({
+        root: {
+            width: '100%'
+        },
+        container: {
+            display: 'flex',
+            flexDirection: 'column',
+            // backgroundColor: 'blue',
+            margin: '10px 20px'
+        },
+        schoolDetails: {
+            height: '30%',
+            padding: '0px 25px',
+            textAlign: 'center',
+        }
 
-})
+    })
+}
 const useStyles = makeStyles(styles)
 
 const School = ({ schoolData, _getSchoolDetails }) => {
@@ -57,7 +61,18 @@ const School = ({ schoolData, _getSchoolDetails }) => {
             <div className={classes.container}>
                 <div id="school-info" style={{ display: 'flex', flexDirection: 'column' }}>
                     <div className={classes.schoolDetails}> <Typography variant="h3">{schoolData.name}</Typography></div>
+                    <Paper style={{ margin: '10px 10px', padding: '20px 20px', width: '70%', display: 'flex', alignSelf: 'center', textAlign: 'justify' }} elevation={0}>
+                        <Typography id="mission-statement" variant="body1">
+                            To Provide an Environment of Learning that enhances Dissemination of Knowledge. To Nurture Research and Innovation for the betterment of Life and Progress of the Nation.
+                            To Provide an Environment of Learning that enhances Dissemination of Knowledge. To Nurture Research and Innovation for the betterment of Life and Progress of the Nation.
+                            To Provide an Environment of Learning that enhances Dissemination of Knowledge. To Nurture Research and Innovation for the betterment of Life and Progress of the Nation.
+                            To Provide an Environment of Learning that enhances Dissemination of Knowledge. To Nurture Research and Innovation for the betterment of Life and Progress of the Nation.
+                            To Provide an Environment of Learning that enhances Dissemination of Knowledge. To Nurture Research and Innovation for the betterment of Life and Progress of the Nation.
+                        </Typography>
+                    </Paper>
+
                     <Address data={schoolData.address}></Address>
+                    <Contact data={schoolData.contact}></Contact>
                 </div>
 
 
