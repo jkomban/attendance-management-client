118e9ce929cdfe39734ac545c4eefa8a60dadac8
John Komban <johnpk007@gmail.com>
2020-05-17 14:08:12 -0400

    Facility component created - Day#9

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -5,6 +5,7 @@ import Dashboard from '../common/components/Dashboard';
 import Classes from '../common/components/Classes';
 import StudentScreen from '../screens/student';
 import SchoolScreen from '../screens/school';
+import FacilityScreen from '../screens/facility'
 
 const Routes = props => {
     // console.log(props)
@@ -14,6 +15,7 @@ const Routes = props => {
             <PrivateRoute exact path="/classes" component={Classes} ></PrivateRoute>
             <PrivateRoute exact path="/students" component={StudentScreen}></PrivateRoute>
             <PrivateRoute exact path="/school" component={SchoolScreen}></PrivateRoute>
+            <PrivateRoute exact path="/facilities" component={FacilityScreen}></PrivateRoute>
         </Switch>
     )
 }
---
+++ src/screens/facility/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core'
+
+const Facility = () => {
+
+    return (
+        <div>Facilitiess..... here</div>
+    )
+}
+
+export default Facility;
