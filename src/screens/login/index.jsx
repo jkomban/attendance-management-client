bb8817b65372dfe67aba4374ed93a0247ac54ec1
John Komban <johnpk007@gmail.com>
2020-05-24 23:36:17 -0400

    Login Controller - Day#15

--- src/screens/login/index.jsx
+++ src/screens/login/index.jsx
@@ -1,64 +1,156 @@
-import React from 'react'
-import Page from '../../common/components/Page';
-import { Paper, Box, TextField, Typography, Card, Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Link as Router, Redirect } from 'react-router-dom';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { authenticate } from '../../store/actions/auth-actions'
 
-const useStyles = makeStyles(theme => {
-    console.log(theme)
-    return {
-        root: {
-            display: 'flex',
-            flexDirection: 'column',
-            alignSelf: 'flex-end',
-            maxWidth: 'fit-content',
-            backgroundColor: theme.palette.primary.light,
-            padding: `${theme.spacing(1)}px ${theme.spacing(1)}px`,
-            margin: `${theme.spacing(1)}px ${theme.spacing(1)}px`
-        },
-        column: {
-            display: 'flex',
-            flexDirection: 'column'
-        }
-    }
-});
 
-const Login = () => {
-    const classes = useStyles()
+function Copyright() {
     return (
-        <Page>
-            <Paper className={classes.root} color="primary" elevation={3}>
-                <Typography >Login Controller</Typography>
-                <Card className={classes.column} variant="outlined">
-                    <TextField
-                        id="username-id"
-                        name="username"
-                        label="User Name/Email ID"
-                        margin="normal"
-                        className={classes.textField}
-                        // value={address.addressLine1 || ''}
-                        onChange={() => { }}
-                    />
-                    <TextField
-                        id="password-id"
-                        name="password"
-                        label="Password"
-                        margin="normal"
-                        className={classes.textField}
-                        // value={address.addressLine1 || ''}
-                        onChange={() => { }}
-                    />
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        size="small"
-                        className={classes.button}
-                        // startIcon={<AddIcon />}
-                        onClick={() => { }}
-                    > Login</Button>
-                </Card>
-            </Paper>
-        </Page>
-    )
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
 }
 
-export default Login;
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+
+const Login = ({ _authenticate, auth }) => {
+    const classes = useStyles();
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+
+    const authenticateUser = () => {
+        console.log(userName)
+        console.log(password)
+        _authenticate({ userName, password })
+    }
+
+
+    return (
+
+        < Container component="main" maxWidth="xs" >
+            {auth.authenticated && <Redirect to="/" />}
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <div>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                        value={userName}
+                        onChange={(e) => setUserName(e.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                    />
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
+                    />
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={authenticateUser}
+                    >
+                        Sign In
+                    </Button>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Router to="/signup">
+                                {/* <Link variant="body2"> */}
+                                {"Don't have an account? Sign Up"}
+                                {/* </Link> */}
+                            </Router>
+                        </Grid>
+                    </Grid>
+                </div>
+            </div>
+            <Box mt={8}>
+                <Copyright />
+            </Box>
+        </Container >
+    );
+}
+
+const mapStateToProps = (state) => {
+    // console.log(state);
+
+    return {
+        auth: state.auth
+    }
+}
+
+const mapDispatachToProps = (dispatch) => {
+    return bindActionCreators({
+        _authenticate: authenticate
+    }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatachToProps)(Login);
