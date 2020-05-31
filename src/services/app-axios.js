24377f08267b5549e8886f6e28c6a938ce847e23
John Komban <johnpk007@gmail.com>
2020-05-30 22:08:03 -0400

    Axios interceptor added for error handling - Day#18

--- src/services/app-axios.js
+++ src/services/app-axios.js
@@ -1,4 +1,30 @@
 import axios from 'axios';
-
 const instance = axios.create({ withCredentials: true });
+
+instance.interceptors.response.use(response => {
+    console.log(response)
+    console.log(`notificationInterceptors():: succcess`)
+    return response;
+}, error => {
+    console.error(`notificationInterceptors():: error`)
+    console.log(error.response)
+    switch (error.response.status) {
+        case 401:
+            console.log("Please login again")
+            error.response.data.type = 'WARNING'
+            break;
+        case 403:
+            console.log("Access Denied: ")
+            error.response.data.type = 'WARNING'
+            break;
+        default:
+            console.log(`Application has caused general failure [${error.response.status}]`)
+            error.response.data.type = 'ERROR'
+            break;
+    }
+
+    return Promise.reject(error)
+})
+
+
 export default instance;
